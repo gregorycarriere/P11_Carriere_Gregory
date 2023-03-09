a5e37ecea682b3753cfa8c7fe33a28778de80af4
Gregory Carriere <gregory.alan.carriere@gmail.com>
2023-03-09 20:32:23 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import App from "./App";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<>
-		<App />
-	</>
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>
 );
