07a308f651062114aec143e22f217c1a75e4328b
Gregory Carriere <gregory.alan.carriere@gmail.com>
2023-03-14 18:15:52 +0100

    error page implementation

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./Error.module.css";
+
 export default function Error() {
 	return (
-		<div className="error">
-			<h1>Error</h1>
+		<div className={styles.container}>
+			<h1 className={styles.error}>404</h1>
+			<p className={styles.description}>
+				<span className={styles.span}>Oups! La page que</span> vous
+				demandez n'existe pas.
+			</p>
+			<Link to="/" className={styles.link}>
+				Retourner sur la page d'accueil
+			</Link>
 		</div>
 	);
 }
