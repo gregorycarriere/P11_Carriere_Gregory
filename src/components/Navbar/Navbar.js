300dfdf1149918518093802c67e59efa95eb3cc7
Gregory Carriere <gregory.alan.carriere@gmail.com>
2023-03-14 18:15:00 +0100

    edit css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
 import styles from "./Navbar.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
 	return (
 		<nav>
-			<Link to="/" className={styles.link}>
+			<NavLink
+				to="/"
+				end
+				className={({ isActive }) =>
+					isActive ? "" : styles.defaultLink
+				}>
 				Accueil
-			</Link>
-			<Link to="/about" className={styles.link}>
+			</NavLink>
+			<NavLink
+				to="/about"
+				end
+				className={({ isActive }) =>
+					isActive ? "" : styles.defaultLink
+				}>
 				A Propos
-			</Link>
+			</NavLink>
 		</nav>
 	);
 }
