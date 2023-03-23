7fbda71897a0758c0baf27412c5c138d5feaf046
Gregory Carriere <gregory.alan.carriere@gmail.com>
2023-03-23 14:00:51 +0100

    fix on ccs and import

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import { ReactComponent as ReactLogo } from "../../assets/logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 import Navbar from "../Navbar/Navbar";
 import styles from "./Header.module.css";
 
 export default function Header() {
 	return (
 		<header>
-			<ReactLogo alt="kasa logo" className={styles.logo} />
+			<Logo alt="kasa logo" className={styles.logo} />
 			<Navbar />
 		</header>
 	);
